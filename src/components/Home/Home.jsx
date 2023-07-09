84f2d0ac9d3d8b143a484fb44eeb9ceceed28341
HIMANSHU498 <KUSHWAHHIMANSHU498@GMAIL.COM>
2023-07-09 22:27:27 +0530

    homepage ui done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import foodImg from "./../../assets/Food.png";
 const Home = () => {
   return (
     <>
@@ -35,6 +36,46 @@ const Home = () => {
           <div className="category-box">
             <h1 className="category-name">Food</h1>
           </div>
+        </div>
+        <div className="stories-container">
+          <h2 className="category-title">Top Stories About food</h2>
+          <div className="story-box">
+            <div className="story-card">
+              <img src={foodImg} alt="foodpic" />
+              <div className="dark-shadow">
+                <h3 className="story-title">Heading comes here</h3>
+                <h4 className="story-description">
+                  Inspirational designs, illustrations, and graphic elements
+                  from the world’s best designers.
+                </h4>
+              </div>
+            </div>
+            <div className="story-card">
+              <img src={foodImg} alt="foodpic" />
+              <h3 className="story-title">Heading comes here</h3>
+              <h4 className="story-description">
+                Inspirational designs, illustrations, and graphic elements from
+                the world’s best designers.
+              </h4>
+            </div>
+            <div className="story-card">
+              <img src={foodImg} alt="foodpic" />
+              <h3 className="story-title">Heading comes here</h3>
+              <h4 className="story-description">
+                Inspirational designs, illustrations, and graphic elements from
+                the world’s best designers.
+              </h4>
+            </div>
+            <div className="story-card">
+              <img src={foodImg} alt="foodpic" />
+              <h3 className="story-title">Heading comes here</h3>
+              <h4 className="story-description">
+                Inspirational designs, illustrations, and graphic elements from
+                the world’s best designers.
+              </h4>
+            </div>
+          </div>
+          <button className="see-more">See more</button>
         </div>
       </div>
     </>
